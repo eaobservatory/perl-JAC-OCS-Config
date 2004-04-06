c56d0e7651003b7a49daf1e268c0e9a7bbd2e526
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-04-06 23:24:34 +0000

    Initial import
    svn path=/trunk/perlmods/; revision=7235

---
+++ lib/JAC/OCS/Config.pm
@@ -0,0 +1,325 @@
+package JAC::OCS::Config;
+
+=head1 NAME
+
+JAC::OCS::Config - Parse and write JCMT OCS Configuration XML
+
+=head1 SYNOPSIS
+
+  use JAC::OCS::Config;
+
+  $cfg = new JAC::OCS::Config( XML => $xml );
+  $cfg = new JAC::OCS::Config( FILE => $filename );
+
+  $inst = $cfg->instrument;
+  $proj = $cfg->projectid;
+
+  $coord = $cfg->centre_coords;
+
+
+=head1 DESCRIPTION
+
+Top-level module for parsing and writing JCMT OCS Configuration XML.
+Also includes code for parsing a UKIRT TCS XML specification, since
+both telescopes use the same PTCS. It is for this reason that the
+C<JAC::> prefix is used for the namespace rather than the more specific
+C<JCMT::> prefix. For UKIRT configuration see the C<UKIRT::Sequence>
+module.
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use XML::LibXML;
+use Time::HiRes qw/ gettimeofday /;
+use Time::Piece qw/ :override /;
+
+use vars qw/ $VERSION /;
+$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+# Overloading
+use overload '""' => "stringify";
+
+
+=head1 METHODS
+
+=head2 Constructors
+
+=over 4
+
+=item B<new>
+
+The constructor takes an XML representation of the config
+as argument and returns a new object.
+
+  $cfg = new JAC::OCS::Config( XML => $xml );
+  $cfg = new JAC::OCS::Config( FILE => $xmlfile );
+
+The argument hash can either refer to an XML string or
+an XML file. If neither is supplied no object will be
+instantiated. If both C<XML> and C<FILE> keys exist, the
+C<XML> key takes priority.
+
+Returns C<undef> if an object can not be constructed.
+
+=cut
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+
+  throw JAC::OCS::Config::Error::BadArgs('Usage : JAC::OCS::Config->new(XML => $xml, FILE => $file)') unless @_;
+
+  my %args = @_;
+
+  my $xml;
+  if (exists $args{XML}) {
+    $xml = $args{XML};
+  } elsif (exists $args{FILE}) {
+    # Dont check for existence - the open will do that for me
+    open my $fh, "<$args{FILE}" or return undef;
+    local $/ = undef; # slurp whole file
+    $xml = <$fh>;
+  } else {
+    warnings::warnif("Neither XML or FILE key specified to constructor");
+    # Nothing of use
+    return undef;
+  }
+
+  # Now convert XML to parse tree
+  my $parser = new XML::LibXML;
+  $parser->validation(0); # switch off validation for noew
+  my $tree = eval { $parser->parse_string( $xml ) };
+  if ($@) {
+    throw JAC::OCS::Config::Error::SpBadStructure("Error whilst parsing configuration XML: $@\n");
+  }
+
+  # Now create Config unblessed hash
+  my $cfg = {
+	     Parser => $parser,
+	     Tree => $tree,
+	    };
+
+  # and create the object
+  bless $cfg, $class;
+
+  # Now need to create the sub-objects
+  # One per sub-system
+
+  # First find the OCS_CONFIG root node
+
+  # Then find all the XXX_CONFIG nodes
+
+  # And for each one, instantiate a new Config::XXX object
+
+  # And store in a hash
+
+  return $cfg;
+}
+
+=back
+
+=head2 Accessor Methods
+
+=over 4
+
+=item B<_tree>
+
+Retrieves or sets the base of the document tree associated
+with the science program. In general this is DOM based. The
+interface does not guarantee the underlying object type
+since that relies on the choice of XML parser.
+
+=cut
+
+sub _tree {
+  my $self = shift;
+  if (@_) { $self->{Tree} = shift; }
+  return $self->{Tree};
+}
+
+
+=back
+
+=head2 General Methods
+
+=over 4
+
+=item B<write_file>
+
+Write the Config XML to disk.
+
+  $outfile = $cfg->write_file();
+  $outfile = $cfg->write_file( $dir, \%opts );
+
+If no directory is specified, the config is written to the directory
+returned by JAC::OCS::Config->outputdir(). The C<outputdir> method is
+a class method, there is no scheme for overriding the default output
+directory per object.
+
+Additional options can be supplied through the optional hash (which must
+be the last argument). Supported keys are:
+
+  chmod => file protection to be used for output files. Default is to
+           use the current umask.
+
+Returns the output filename with path information for the file written in
+the root directory.
+
+Currently, the config is written to the output directory and each
+sub-directory within that directory.
+
+=cut
+
+sub write_file {
+  my $self = shift;
+
+  # Look for hash ref as last arg and read options
+  my $newopts;
+  $newopts = pop() if ref($_[-1]) eq 'HASH';
+  my %options = ();
+  %options = (%options, %$newopts) if $newopts;
+
+  # Now if we have anything left its a directory
+  my $TRANS_DIR = shift;
+
+  # The interface currently suggests that I write one copy into TRANS_DIR
+  # itself and another copy of the XML file into each of the directories
+  # found in TRANS_DIR
+  $TRANS_DIR = $self->outputdir unless defined $TRANS_DIR;
+
+  opendir my $dh, $TRANS_DIR ||
+    throw JAC::OCS::Config::Error::FatalError("Error opening OCS config output directory '$TRANS_DIR': $!");
+
+  # Get all the dirs (making sure curdir is first in the list
+  # so that when things paths are formed we end up in TRANS_DIR)
+  # except hidden dirs [assume unix hidden definition XXX]
+  my @dirs = (File::Spec->curdir,
+	      grep { -d File::Spec->catdir($TRANS_DIR,$_) && $_ !~ /^\./ } readdir($dh));
+
+  # Format is acsis_YYYYMMDD_HHMMSSuuuuuu.xml
+  #  where uuuuuu is microseconds
+
+  my ($sec, $mic_sec) = gettimeofday();
+  my $ut = gmtime( $sec );
+
+  # Rather than worry that the computer is so fast in looping that we might
+  # reuse milli-seconds (and therefore have to check that we are not opening
+  # a file that has previously been created) micro-seconds in the filename
+  my $cname = "acsis_". $ut->strftime("%Y%m%d_%H%M%S") .
+    "_".sprintf("%06d",$mic_sec) .
+      ".xml";
+
+  my $storename;
+  for my $dir (@dirs) {
+
+    my $fullname = File::Spec->catdir( $TRANS_DIR, $dir, $cname );
+    print "Writing config to $fullname\n";
+
+    # First time round, store the filename for later return
+    $storename = $fullname unless defined $storename;
+
+    # Open it [without checking to see if we are clobbering a pre-existing file]
+    open my $fh, "> $fullname" ||
+      throw JAC::OCS::Config::Error::FatalError("Error opening config output file $fullname: $!");
+    print $fh "$self";
+    close ($fh) ||
+      throw JAC::OCS::Config::Error::FatalError("Error closing config output file $fullname: $!");
+
+    chmod $options{chmod}, $fullname
+      if exists $options{chmod};
+
+  }
+
+  return $storename;
+}
+
+=item B<instrument>
+
+Return the instrument (aka Front End) associated with this configuration.
+
+=cut
+
+sub instrument {
+  warn "instrument method Not yet implemented\n";
+  return "ACSIS";
+}
+
+=item B<duration>
+
+Estimated duration of the observation resulting from this configuration.
+This must be an estimate because of uncertainties in slew time and
+the possibility that a raster map may not use predictable scanning.
+
+=cut
+
+sub duration {
+  warn "Observation duration unknown\n";
+  return Time::Seconds->new(10);
+}
+
+=item B<stringify>
+
+Convert the Science Program object into XML.
+
+  $xml = $sp->stringify;
+
+This method is also invoked via a stringification overload.
+
+  print "$sp";
+
+=cut
+
+sub stringify {
+  my $self = shift;
+  $self->_tree->toString;
+}
+
+
+=back
+
+=head2 Class Methods
+
+=over 4
+
+=item B<telescope>
+
+Return the telescope name associated with this Config.
+Always returns "JCMT" if no other information is available.
+
+=cut
+
+sub telescope { return "JCMT" }
+
+=back
+
+=head1 AUTHOR
+
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2004 Particle Physics and Astronomy Research Council.
+All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
+
+=head1 SEE ALSO
+
+L<SCUBA::ODF>, L<UKIRT::Sequence>
+
+=cut
+
+1;
---
+++ lib/JAC/OCS/Config/Error.pm
@@ -0,0 +1,262 @@
+package JAC::OCS::Config::Error;
+
+=head1 NAME
+
+JAC::OCS::Config::Error - Exception handling in an object orientated manner.
+
+=head1 SYNOPSIS
+
+    use JAC::OCS::Config::Error qw /:try/;
+    use JAC::OCS::Config::Constants qw /:status/;
+
+    # throw an error to be caught
+    throw JAC::OCS::Config::Error::AuthenticationFail( $message, OMP__AUTHFAIL );
+    throw JAC::OCS::Config::Error::FatalError( $message, OMP__FATAL );
+
+    # record and then retrieve an error
+    do_stuff();
+    my $Error = JAC::OCS::Config::Error->prior;
+    JAC::OCS::Config::Error->flush if defined $Error;
+
+    sub do_stuff {
+        record JAC::OCS::Config::Error::FatalError( $message, OMP__FATAL);
+    }
+
+    # try and catch blocks
+    try {
+       stuff();
+    }
+    catch JAC::OCS::Config::Error::FatalError with 
+    {
+        # its a fatal error
+        my $Error = shift;
+	orac_exit_normally($Error);
+    }
+    otherwise 
+    {
+       # this block catches croaks and other dies
+       my $Error = shift;
+       orac_exit_normally($Error);
+
+    }; # Dont forget the trailing semi-colon to close the catch block
+
+=head1 DESCRIPTION
+
+C<JAC::OCS::Config::Error> inherits from the L<Error|Error> class and more
+documentation about the (many) features present in the module but
+currently unused by the OMP can be found in the documentation for that
+module.
+
+As with the C<Error> package, C<JAC::OCS::Config::Error> provides two
+interfaces.  Firstly it provides a procedural interface to exception
+handling, and secondly C<JAC::OCS::Config::Error> is a base class for
+exceptions that can either be thrown, for subsequent catch, or can
+simply be recorded.
+
+=head1 PROCEDURAL INTERFACE
+
+C<JAC::OCS::Config::Error> exports subroutines to perform exception
+handling. These will be exported if the C<:try> tag is used in the
+C<use> line.
+
+=over 4
+
+=item try BLOCK CLAUSES
+
+C<try> is the main subroutine called by the user. All other
+subroutines exported are clauses to the try subroutine.
+
+The BLOCK will be evaluated and, if no error is throw, try will return
+the result of the block.
+
+C<CLAUSES> are the subroutines below, which describe what to do in the
+event of an error being thrown within BLOCK.
+
+=item catch CLASS with BLOCK
+
+This clauses will cause all errors that satisfy
+C<$err-E<gt>isa(CLASS)> to be caught and handled by evaluating
+C<BLOCK>.
+
+C<BLOCK> will be passed two arguments. The first will be the error
+being thrown. The second is a reference to a scalar variable. If this
+variable is set by the catch block then, on return from the catch
+block, try will continue processing as if the catch block was never
+found.
+
+To propagate the error the catch block may call C<$err-E<gt>throw>
+
+If the scalar reference by the second argument is not set, and the
+error is not thrown. Then the current try block will return with the
+result from the catch block.
+
+=item otherwise BLOCK
+
+Catch I<any> error by executing the code in C<BLOCK>
+
+When evaluated C<BLOCK> will be passed one argument, which will be the
+error being processed.
+
+Only one otherwise block may be specified per try block
+
+=back
+
+=head1 CLASS INTERFACE
+
+=head2 CONSTRUCTORS
+
+The C<JAC::OCS::Config::Error> object is implemented as a HASH. This
+HASH is initialized with the arguments that are passed to it's
+constructor. The elements that are used by, or are retrievable by the
+C<JAC::OCS::Config::Error> class are listed below, other classes may
+add to these.
+
+	-file
+	-line
+	-text
+	-value
+
+If C<-file> or C<-line> are not specified in the constructor arguments
+then these will be initialized with the file name and line number
+where the constructor was called from.
+
+The C<JAC::OCS::Config::Error> package remembers the last error
+created, and also the last error associated with a package.
+
+=over 4
+
+=item throw ( [ ARGS ] )
+
+Create a new C<JAC::OCS::Config::Error> object and throw an error,
+which will be caught by a surrounding C<try> block, if there is
+one. Otherwise it will cause the program to exit.
+
+C<throw> may also be called on an existing error to re-throw it.
+
+=item with ( [ ARGS ] )
+
+Create a new C<JAC::OCS::Config::Error> object and returns it. This
+is defined for syntactic sugar, eg
+
+    die with JAC::OCS::Config::Error::FatalError ( $message, OMP__FATAL );
+
+=item record ( [ ARGS ] )
+
+Create a new C<JAC::OCS::Config::Error> object and returns it. This
+is defined for syntactic sugar, eg
+
+  record JAC::OCS::Config::Error::AuthenticationFail ( $message, OMP__ABORT )
+	and return;
+
+=back
+
+=head2 METHODS
+
+=over 4
+
+=item prior ( [ PACKAGE ] )
+
+Return the last error created, or the last error associated with
+C<PACKAGE>
+
+    my $Error = JAC::OCS::Config::Error->prior;
+
+
+=back
+
+=head2 OVERLOAD METHODS
+
+=over 4
+
+=item stringify
+
+A method that converts the object into a string. By default it returns
+the C<-text> argument that was passed to the constructor, appending
+the line and file where the exception was generated.
+
+=item value
+
+A method that will return a value that can be associated with the
+error. By default this method returns the C<-value> argument that was
+passed to the constructor.
+
+=back
+
+=head1 PRE-DEFINED ERROR CLASSES
+
+=over 4
+
+=item B<JAC::OCS::Config::Error::Authentication>
+
+The password provided could not be authenticated. Also encoded
+in the constant C<OMP__AUTHFAIL>
+
+=item B<JAC::OCS::Config::Error::BadArgs>
+
+Method was called with incorrect arguments.
+
+=item B<JAC::OCS::Config::Error::DirectoryNotFound>
+
+The requested directory could not be found.
+
+=item B<JAC::OCS::Config::Error::FatalError>
+
+Used when we have no choice but to abort but using a non-standard
+reason. It's constructor takes two arguments. The first is a text
+value, the second is a numeric value, C<OMP__FATAL>. These values are
+what will be returned by the overload methods.
+
+=item B<JAC::OCS::Config::Error::XMLBadStructure>
+
+The configuration XML was not valid.
+
+=item B<JAC::OCS::Config::Error::XMLEmpty>
+
+No configs were present in the XML.
+
+=back
+
+
+=head1 AUTHORS
+
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>,
+Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001-2004 Particle Physics and Astronomy Research Council.
+All Rights Reserved.
+
+=cut
+
+use Error;
+use warnings;
+use strict;
+
+use vars qw/$VERSION/;
+
+$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+# flush method added to the base class
+use base qw/ Error::Simple /;
+
+package JAC::OCS::Config::Error::Authentication;
+use base qw/ JAC::OCS::Config::Error /;
+
+package JAC::OCS::Config::Error::BadArgs;
+use base qw/ JAC::OCS::Config::Error /;
+
+package JAC::OCS::Config::Error::DirectoryNotFound;
+use base qw/ JAC::OCS::Config::Error /;
+
+package JAC::OCS::Config::Error::FatalError;
+use base qw/ JAC::OCS::Config::Error /;
+
+package JAC::OCS::Config::Error::XMLBadStructure;
+use base qw/ JAC::OCS::Config::Error /;
+
+package JAC::OCS::Config::Error::XMLEmpty;
+use base qw/ JAC::OCS::Config::Error /;
+
+1;
+
